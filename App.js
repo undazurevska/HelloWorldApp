a2f9ad9b5abb5534cb60d761f0b05220fc8739e7
undazurevska <95476474+undazurevska@users.noreply.github.com>
2021-12-03 16:03:07 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -10,11 +10,22 @@ const HelloWorldApp = () => {
         alignItems: 'center',
         padding: 8,
       }}>
+      <Text style={{
+        fontWeight: 'bold',
+        fontSize: 20,
+      }}
+      >Hello World
+      </Text>
       <Text
         style={{
-          fontSize: 20,
+          fontSize: 15,
+          fontweight: 300,
+          margin: 20,
+          lineHeight: 25,
         }}>
-      Hello World from Group 1 member: Unda Zurevska!  {'\n'}This is my Unda Zurevska first React Native application!
+      Hello World from Group 1{'\n'}Member: 
+      <Text style={{fontWeight: "bold"}}> Unda Zurevska</Text>  
+      {'\n'}This is my Unda Zurevska first React Native application!
       </Text>
      <Image source={require('./assets/cat.png')} />
     </View>
